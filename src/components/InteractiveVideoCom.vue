4e4f64bb15f1037e840faaf980dc9a3d87c17e7b
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-01-22 18:57:02 +0800

    互动视频控件

--- src/components/InteractiveVideoCom.vue
+++ src/components/InteractiveVideoCom.vue
@@ -3,24 +3,19 @@
     <div class="demonstration">{{ title }}</div>
     <div class="video">
       <div id="mse"></div>
-      <div class="control">
-        <div class="button">
-          <el-button type="primary">主要按钮</el-button>
-        </div>
-        <div class="button">
-          <el-button type="primary">主要按钮</el-button>
+      <div class="control" v-if="showHandle">
+        <div class="button" v-for="(item, index) in controlList" :key="index">
+          <el-button type="primary" @click="switchVideo(item)">{{ item.title }}</el-button>
         </div>
       </div>
     </div>
-
-    <!-- <video style="width: 100%; height: 100%" :src="url" controls :poster="poster" ref="video" @onplay="onplay">
-      <source :src="url" />
-    </video> -->
   </div>
 </template>
 
 <script>
 import Player from "xgplayer";
+import { Hash } from "@/utils/generateHash";
+import { valInDeep } from "@/utils/arrayUtil";
 export default {
   name: "VideoAndTitle",
   props: {
@@ -41,12 +36,19 @@ export default {
   },
   data() {
     return {
+      showHandle: false,
       url: "",
       poster: "",
-      player: null
+      player: null,
+      controlList: [],
+      key: ""
     };
   },
   computed: {},
+  created() {
+    this.createHashTree(this.list);
+    console.log(this.list, "this.list");
+  },
   mounted() {
     this.init();
     this.player = new Player({
@@ -54,22 +56,38 @@ export default {
       url: this.url,
       fluid: true,
       videoInit: true,
-      poster: this.poster
+      poster: this.poster,
+      ignores: []
     });
     this.player.once("ready", () => {
       this.initEvent();
     });
   },
   methods: {
+    createHashTree(list) {
+      for (let i = 0; i < list.length; i++) {
+        list[i].key = Hash.create(32);
+        if (list[i].children && list[i].children.length != 0) {
+          let childrenList = list[i].children;
+          this.createHashTree(childrenList);
+        }
+      }
+    },
     init() {
       if (this.list.length != 0) {
         this.url = this.list[0].url;
         this.poster = this.list[0].poster;
+        this.setControlList(this.list[0].children);
+        this.key = this.list[0].key;
       }
+    },
+    setControlList(list) {
+      this.controlList = list;
     },
     initEvent() {
       this.player.on("play", this.play);
       this.player.on("ended", this.ended);
+      this.player.on("timeupdate", this.timeupdate);
     },
     play() {
       console.log("播放开始");
@@ -77,15 +95,45 @@ export default {
     ended() {
       console.log("播放结束");
     },
-    switchVideo(url) {
-      this.player.src = url;
+    timeupdate() {
+      let currentTime = this.player.currentTime;
+      let duration = this.player.duration;
+      console.log(currentTime, duration - 0.2);
+      if (currentTime > duration - 0.3) {
+        //防止播放完成黑屏
+        this.player.pause();
+        this.finderChildren(this.key);
+        this.showhandle(true);
+      }
+    },
+    finderChildren(key) {
+      const target = valInDeep(this.list, key, "key", "children");
+      console.log(target, "target");
+      if (target.length != 0) {
+        if (target[0].children && target[0].children.length != 0) {
+          this.setControlList(target[0].children);
+        }
+      }
+    },
+    showhandle(state) {
+      this.showHandle = state;
+    },
+    switchVideo(item) {
+      this.player.src = item.url;
+      this.key = item.key;
       this.player.play();
+      this.showhandle(false);
     }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.xgplayer-skin-default.xgplayer-pause .xgplayer-start {
+  display: none;
+}
+</style>
+
 <style scoped lang="less">
 .block {
   width: 60%;
@@ -113,7 +161,7 @@ export default {
       justify-content: center;
       position: absolute;
       bottom: 70px;
-      z-index: 100;
+      z-index: 200;
       .button {
         flex: 1;
       }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/element-ui";
+import "./plugins/random-hash";
 Vue.config.productionTip = false;
 
 new Vue({
